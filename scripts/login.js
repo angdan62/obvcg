da8217e7cfed6dacb49c64fcd220017e1e72e9e1
angdan62 <angdan62@gmail.com>
2024-12-07 15:13:51 -0600

    se anaden alertas y nueva contraseña

--- scripts/login.js
+++ scripts/login.js
@@ -5,9 +5,19 @@ const passwordInput = document.querySelector(".password");
 const submitInput = document.querySelector(".login");
 const forgetPassword = document.querySelector(".forgetPassword");
 const alertMessage = document.querySelector(".alertMessage");
-const storedPasswordHash = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d9";
-// const keyUser = "3ea87a56da3844b420ec2925ae922bc731ec16a4fc44dcbeafdad49b0e61d39c";
+// const storedPasswordHash = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d9";
+const storedPasswordHash = "7bbeea8db68311e680e01510a3bf88e4bc83438dbffce0f4a83607cc6168c215";
+
+console.log(encryptSHA256("240224"))
 //CONTROL PARA CUANDO SE OLVIDÓ LA CONTRASEÑA
+
+forgetPassword.addEventListener("click", ()=>{
+    Swal.fire({
+        title: "¿Has olvidado tu contraseña?",
+        text: "Recuerda uno de los dos días más especiales (ddmmaa)",
+        icon: "question"
+      });
+});
 
 
 
